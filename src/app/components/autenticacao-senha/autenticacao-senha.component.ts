cfda89437976f484b90152ffba463eb4aeb70b9c
Leticia Lima <leticia_lele.lima@hotmail.com>
2025-01-20 14:29:23 -0300

    adicionando e modificando a tela de verificar a senha

--- src/app/components/autenticacao-senha/autenticacao-senha.component.ts
+++ src/app/components/autenticacao-senha/autenticacao-senha.component.ts
@@ -5,13 +5,27 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-autenticacao-senha',
   templateUrl: './autenticacao-senha.component.html',
-  styleUrl: './autenticacao-senha.component.scss',
+  styleUrls: ['./autenticacao-senha.component.scss'],
 })
 export class AutenticacaoSenhaComponent {
+  password: string[] = [];
+
   constructor(private router: Router) {}
 
   avancarParaSenha() {
-    // Aqui você pode fazer validações, se necessário
-    this.router.navigate(['/dashboard']); // Redireciona para a tela de autenticação de senha
+    this.router.navigate(['/dashboard']);
+  }
+
+  addDigit(digit: number) {
+    if (this.password.length < 4) {
+      this.password.push(digit.toString());
+      if (this.password.length === 4) {
+        setTimeout(() => this.avancarParaSenha(), 500); // Pequeno atraso para o usuário ver o último dígito
+      }
+    }
+  }
+
+  clearPassword() {
+    this.password = [];
   }
 }
